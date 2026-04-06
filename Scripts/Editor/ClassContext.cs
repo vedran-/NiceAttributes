c46f5992b1c7bbc2dbfe833b038a1d7d638d2c74
Vedran Manojlovic <vmanojlo@gmail.com>
2026-04-06 09:53:15 +0200

    refactor: extract MemberOrderer from ClassContext

--- Scripts/Editor/ClassContext.cs
+++ Scripts/Editor/ClassContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using NiceAttributes.Editor.Ordering;
 using NiceAttributes.Editor.PropertyDrawers;
 using NiceAttributes.Editor.Utility;
 using NiceAttributes.Interfaces;
@@ -56,7 +57,7 @@ namespace NiceAttributes.Editor
             if (!ctx.HasNiceAttributes) return ctx;
 
             // Order class members, as they appear in the source code file
-            var orderedMembers = GetOrderedMembersByLineNumber(members);
+            var orderedMembers = MemberOrderer.Order(members);
 
             var displayTree = BuildDisplayTree(orderedMembers);
             ctx._displayedMembers = displayTree.members;
@@ -207,50 +208,6 @@ namespace NiceAttributes.Editor
             }
 
             return members;
-        }
-
-        private static IEnumerable<ClassItem> GetOrderedMembersByLineNumber(List<ClassItem> members)
-        {
-            // For members which don't have line numbers, select a number between their neighbours which do have line numbers
-            // But make sure that only use neighbours of the same type!
-            MemberTypes lastType = 0;
-            float minValue = -1;
-            for( int idx = 0; idx < members.Count; idx++ )
-            {
-                var m = members[idx];
-                if( m.lineNumber < 0 )
-                {
-                    // If member type changed from last member, then reset last min value
-                    if( m.memberInfo.MemberType != lastType ) minValue = -1;
-
-                    // Find next member of the same type, which has line number set.
-                    var maxValue = -1f;
-                    for( int i = idx + 1; i < members.Count; i++ ) {
-                        if( members[i].memberInfo.MemberType != m.memberInfo.MemberType ) break;   // Not in our group any more, stop the search
-                        if( members[i].lineNumber < 0 ) continue;
-                        maxValue = members[i].lineNumber;
-                        break;
-                    }
-
-                    float GetNewLineNumber()
-                    {
-                        // If no member of this type has line number set, return number
-                        if( minValue < 0 && maxValue < 0 ) return (int)m.memberInfo.MemberType * 10f;
-                        if( minValue < 0 && maxValue >= 0 ) return maxValue - 1f;
-                        if( maxValue < 0 && minValue >= 0 ) return minValue + 1f;
-                        return (minValue + maxValue) / 2f;
-                    }
-
-                    // Set new calculated line number - this will be used for sorting
-                    m.lineNumber = GetNewLineNumber();
-                }
-
-                lastType = m.memberInfo.MemberType;
-                minValue = m.lineNumber;
-            }
-
-            // Finally, order all members by line numbers
-            return members.OrderBy( m => m.lineNumber );
         }
 
         private static (Dictionary<string, GroupInfo> groups, List<ClassItem> members) BuildDisplayTree( IEnumerable<ClassItem> orderedMembers )
---
+++ Scripts/Editor/Ordering/MemberOrderer.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace NiceAttributes.Editor.Ordering
+{
+    public static class MemberOrderer
+    {
+        /// <summary>
+        /// Orders members by line number, interpolating line numbers for members that don't have them.
+        /// For members which don't have line numbers, select a number between their neighbours which do have line numbers.
+        /// But make sure that only use neighbours of the same type!
+        /// </summary>
+        public static IEnumerable<ClassContext.ClassItem> Order(List<ClassContext.ClassItem> members)
+        {
+            MemberTypes lastType = 0;
+            float minValue = -1;
+            for( int idx = 0; idx < members.Count; idx++ )
+            {
+                var m = members[idx];
+                if( m.lineNumber < 0 )
+                {
+                    // If member type changed from last member, then reset last min value
+                    if( m.memberInfo.MemberType != lastType ) minValue = -1;
+
+                    // Find next member of the same type, which has line number set.
+                    var maxValue = -1f;
+                    for( int i = idx + 1; i < members.Count; i++ ) {
+                        if( members[i].memberInfo.MemberType != m.memberInfo.MemberType ) break;   // Not in our group any more, stop the search
+                        if( members[i].lineNumber < 0 ) continue;
+                        maxValue = members[i].lineNumber;
+                        break;
+                    }
+
+                    float GetNewLineNumber()
+                    {
+                        // If no member of this type has line number set, return number
+                        if( minValue < 0 && maxValue < 0 ) return (int)m.memberInfo.MemberType * 10f;
+                        if( minValue < 0 && maxValue >= 0 ) return maxValue - 1f;
+                        if( maxValue < 0 && minValue >= 0 ) return minValue + 1f;
+                        return (minValue + maxValue) / 2f;
+                    }
+
+                    // Set new calculated line number - this will be used for sorting
+                    m.lineNumber = GetNewLineNumber();
+                }
+
+                lastType = m.memberInfo.MemberType;
+                minValue = m.lineNumber;
+            }
+
+            // Finally, order all members by line numbers
+            return members.OrderBy( m => m.lineNumber );
+        }
+    }
+}
