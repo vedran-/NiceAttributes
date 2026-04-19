9b018419f84c1d029f81bccd891026f69f1b7174
Vedran Manojlovic <vmanojlo@gmail.com>
2026-04-19 02:06:57 +0200

    fix(HIGH-4): isolate FoldoutAttribute state per target object
    Foldout key now includes target instance ID via
    PropertyDrawPipeline.CurrentTarget, so each object has independent
    fold state. Old keys (GroupName-only) are no longer used; new keys
    use NiceAttributes_Foldout_{instanceId}_{GroupName} prefix.

--- Scripts/Core/GroupAttributes/FoldoutAttribute.cs
+++ Scripts/Core/GroupAttributes/FoldoutAttribute.cs
@@ -1,4 +1,8 @@
-﻿using System;
+#if UNITY_EDITOR
+using NiceAttributes.Editor.Utility;
+#endif
+
+using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using UnityEditor;
@@ -9,40 +13,33 @@ namespace NiceAttributes
     [Conditional("UNITY_EDITOR")]
     public class FoldoutAttribute : BaseGroupAttribute
     {
-#if UNITY_EDITOR
-        private bool _foldedOut = true;
-#endif
-
-        //var id = $"{this.target.GetInstanceID()}.{Name}";    // TODO
-        string id => GroupName;
+        private const string _prefsPrefix = "NiceAttributes_Foldout_";
 
         public FoldoutAttribute(string groupName = "", [CallerLineNumber] int lineNumber = 0)
             : base(groupName, lineNumber)
         {
-#if UNITY_EDITOR
-            _foldedOut = EditorPrefs.GetBool(id, true);
-#endif
         }
 
 #if UNITY_EDITOR
+        private string GetFoldoutKey()
+        {
+            var targetId = PropertyDrawPipeline.CurrentTarget?.GetInstanceID() ?? 0;
+            return _prefsPrefix + targetId + "_" + GroupName;
+        }
+
         private protected override bool OnGUI_GroupStart()
         {
             var rect = EditorGUILayout.BeginVertical();
-
-            // Fill the background, if set
             if (GroupBackColor.HasValue()) GUIUtil.FillRect(rect, GroupBackColor.ToColor());
-
             var label = Title ?? GroupName;
-            var folded = EditorGUILayout.Foldout(_foldedOut, GetLabel(), true);
-            if (folded != _foldedOut)
+            var key = GetFoldoutKey();
+            var folded = EditorPrefs.GetBool(key, true);
+            var foldedOut = EditorGUILayout.Foldout(folded, GetLabel(), true);
+            if (foldedOut != folded)
             {
-                // Value changed
-                _foldedOut = folded;
-                EditorPrefs.SetBool(id, folded);
+                EditorPrefs.SetBool(key, foldedOut);
             }
-
-            // Return true to draw elements of the group, or false not to draw them
-            return _foldedOut;
+            return foldedOut;
         }
 
         private protected override void OnGUI_GroupEnd()
